6e32f35e2f92861934abc63f6cfc2d21b1291f7b
BertSec <79832078+Bert-Sec@users.noreply.github.com>
2026-03-16 02:48:34 -0500

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,223 +1,23 @@
 from __future__ import annotations
-
-import re
-# We use curl_cffi for browser impersonation to bypass Reddit/YouTube blocks
 from curl_cffi import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from dataclasses import dataclass
 from typing import Dict, List, Optional
 
-# --- CONFIGURATION ---
-
 PLATFORMS: Dict[str, str] = {
-    "GitHub": "https://github.com/{}",
-    "GitLab": "https://gitlab.com/{}",
-    "Bitbucket": "https://bitbucket.org/{}",
-    "Codeberg": "https://codeberg.org/{}",
-    "SourceHut": "https://git.sr.ht/~{}",
-    "Gitea": "https://gitea.com/{}",
-    "Forgejo": "https://codeberg.org/{}",
-    "Launchpad": "https://launchpad.net/~{}",
-    "Hugging Face": "https://huggingface.co/{}",
-    "Replit": "https://replit.com/@{}",
-    "CodePen": "https://codepen.io/{}",
-    "JSFiddle": "https://jsfiddle.net/user/{}/",
-    "StackBlitz": "https://stackblitz.com/@{}",
-    "Glitch": "https://glitch.com/@{}",
-    "Observable": "https://observablehq.com/@{}",
-    "Kaggle": "https://www.kaggle.com/{}",
-    "PyPI": "https://pypi.org/user/{}",
-    "npm": "https://www.npmjs.com/~{}",
-    "Docker Hub": "https://hub.docker.com/u/{}",
-    "Crates.io": "https://crates.io/users/{}",
-    "RubyGems": "https://rubygems.org/profiles/{}",
-    "Packagist": "https://packagist.org/users/{}",
-    "CPAN": "https://metacpan.org/author/{}",
-    "Dev.to": "https://dev.to/{}",
-    "Hashnode": "https://{}.hashnode.dev",
-    "Medium": "https://medium.com/@{}",
-    "Substack": "https://{}.substack.com",
-    "Blogger": "https://{}.blogspot.com",
-    "WordPress": "https://{}.wordpress.com",
-    "Ghost": "https://{}.ghost.io",
-    "Wix": "https://{}.wixsite.com",
-    "About.me": "https://about.me/{}",
-    "Linktree": "https://linktr.ee/{}",
-    "Carrd": "https://{}.carrd.co",
-    "Ko-fi": "https://ko-fi.com/{}",
-    "Buy Me a Coffee": "https://www.buymeacoffee.com/{}",
-    "Patreon": "https://www.patreon.com/{}",
-    "Gumroad": "https://{}.gumroad.com",
-    "Product Hunt": "https://www.producthunt.com/@{}",
-    "Indie Hackers": "https://www.indiehackers.com/{}",
-    "AngelList Legacy": "https://angel.co/u/{}",
-    "Wellfound": "https://wellfound.com/u/{}",
-    "Crunchbase People": "https://www.crunchbase.com/person/{}",
-    "Freelancer": "https://www.freelancer.com/u/{}",
-    "Fiverr": "https://www.fiverr.com/{}",
-    "Upwork": "https://www.upwork.com/freelancers/~{}",
-    "99designs": "https://99designs.com/profiles/{}",
-    "Envato": "https://elements.envato.com/user/{}",
-    "Codecanyon": "https://codecanyon.net/user/{}",
-    "ThemeForest": "https://themeforest.net/user/{}",
-    "Behance": "https://www.behance.net/{}",
-    "Dribbble": "https://dribbble.com/{}",
-    "DeviantArt": "https://www.deviantart.com/{}",
-    "ArtStation": "https://www.artstation.com/{}",
-    "Pixiv": "https://www.pixiv.net/en/users/{}",
-    "500px": "https://500px.com/p/{}",
-    "Flickr": "https://www.flickr.com/people/{}",
-    "VSCO": "https://vsco.co/{}/gallery",
-    "Unsplash": "https://unsplash.com/@{}",
-    "Pexels": "https://www.pexels.com/@{}/",
-    "Canva": "https://www.canva.com/{}",
-    "Instagram": "https://www.instagram.com/{}/",
-    "Threads": "https://www.threads.net/@{}",
-    "X": "https://x.com/{}",
-    "Facebook": "https://www.facebook.com/{}",
-    "TikTok": "https://www.tiktok.com/@{}",
-    "Snapchat": "https://www.snapchat.com/add/{}",
-    "Pinterest": "https://www.pinterest.com/{}/",
-    "Reddit": "https://www.reddit.com/user/{}",
-    "Tumblr": "https://{}.tumblr.com",
-    "Mastodon Social": "https://mastodon.social/@{}",
-    "Mastodon Online": "https://mastodon.online/@{}",
-    "Pixelfed Social": "https://pixelfed.social/{}",
-    "VK": "https://vk.com/{}",
-    "OK": "https://ok.ru/{}",
-    "Weibo": "https://www.weibo.com/{}",
-    "Naver Blog": "https://blog.naver.com/{}",
-    "Quora": "https://www.quora.com/profile/{}",
-    "Bluesky": "https://bsky.app/profile/{}",
-    "Discord Invite Vanity": "https://discord.com/users/{}",
-    "Telegram": "https://t.me/{}",
-    "Keybase": "https://keybase.io/{}",
-    "Disqus": "https://disqus.com/by/{}/",
-    "Gravatar": "https://gravatar.com/{}",
-    "Hacker News": "https://news.ycombinator.com/user?id={}",
-    "Lobsters": "https://lobste.rs/u/{}",
-    "Stack Overflow": "https://stackoverflow.com/users/{}",
-    "Stack Exchange": "https://stackexchange.com/users/{}",
-    "Super User": "https://superuser.com/users/{}",
-    "Server Fault": "https://serverfault.com/users/{}",
-    "Ask Ubuntu": "https://askubuntu.com/users/{}",
-    "LeetCode": "https://leetcode.com/{}",
-    "HackerRank": "https://www.hackerrank.com/{}",
-    "Codewars": "https://www.codewars.com/users/{}",
-    "CodeChef": "https://www.codechef.com/users/{}",
-    "AtCoder": "https://atcoder.jp/users/{}",
-    "Topcoder": "https://www.topcoder.com/members/{}",
-    "Exercism": "https://exercism.org/profiles/{}",
-    "FreeCodeCamp": "https://www.freecodecamp.org/{}",
-    "GeeksforGeeks": "https://auth.geeksforgeeks.org/user/{}/",
-    "TryHackMe": "https://tryhackme.com/p/{}",
-    "Hack The Box": "https://app.hackthebox.com/profile/{}",
-    "Root Me": "https://www.root-me.org/{}",
-    "CTFtime Team/User": "https://ctftime.org/team/{}",
-    "Hackaday": "https://hackaday.io/{}",
-    "Instructables": "https://www.instructables.com/member/{}/",
-    "Roblox": "https://www.roblox.com/users/profile?username={}",
-    "Steam": "https://steamcommunity.com/id/{}",
-    "Xbox Gamertag": "https://xboxgamertag.com/search/{}",
-    "PlayStation Profiles": "https://psnprofiles.com/{}",
-    "Nintendo Life": "https://www.nintendolife.com/users/{}",
-    "Chess.com": "https://www.chess.com/member/{}",
-    "Lichess": "https://lichess.org/@/{}",
-    "Speedrun.com": "https://www.speedrun.com/user/{}",
     "Twitch": "https://www.twitch.tv/{}",
     "YouTube": "https://www.youtube.com/@{}",
-    "Vimeo": "https://vimeo.com/{}",
-    "Dailymotion": "https://www.dailymotion.com/{}",
-    "SoundCloud": "https://soundcloud.com/{}",
-    "Bandcamp": "https://{}.bandcamp.com",
-    "Last.fm": "https://www.last.fm/user/{}",
-    "Mixcloud": "https://www.mixcloud.com/{}",
-    "Spotify User": "https://open.spotify.com/user/{}",
-    "Deezer": "https://www.deezer.com/us/profile/{}",
-    "Genius": "https://genius.com/{}",
-    "IMDb": "https://www.imdb.com/user/{}",
-    "Letterboxd": "https://letterboxd.com/{}/",
-    "Goodreads": "https://www.goodreads.com/{}",
-    "StoryGraph": "https://app.thestorygraph.com/profile/{}",
-    "Wattpad": "https://www.wattpad.com/user/{}",
-    "Archive of Our Own": "https://archiveofourown.org/users/{}",
-    "FanFiction": "https://www.fanfiction.net/u/{}",
-    "Tripadvisor": "https://tripadvisor.com/members/{}",
-    "Tripadvisor Profile": "https://www.tripadvisor.com/Profile/{}",
-    "AllTrails": "https://www.alltrails.com/members/{}",
-    "Strava": "https://www.strava.com/athletes/{}",
-    "Garmin Connect": "https://connect.garmin.com/modern/profile/{}",
-    "Runkeeper": "https://runkeeper.com/user/{}",
-    "Untappd": "https://untappd.com/user/{}",
-    "Vivino": "https://www.vivino.com/users/{}",
-    "Couchsurfing": "https://www.couchsurfing.com/people/{}",
-    "TripIt": "https://www.tripit.com/people/{}",
-    "Airbnb User": "https://www.airbnb.com/users/show/{}",
-    "Venmo": "https://venmo.com/{}",
-    "Cash App": "https://cash.app/${}",
-    "PayPal.me": "https://paypal.me/{}",
-    "Etsy": "https://www.etsy.com/people/{}",
-    "Poshmark": "https://poshmark.com/closet/{}",
-    "Depop": "https://www.depop.com/{}",
-    "Mercari": "https://www.mercari.com/u/{}/",
-    "eBay": "https://www.ebay.com/usr/{}",
-    "Amazon Wishlist": "https://www.amazon.com/hz/wishlist/ls/{}",
-    "Rakuten Viki": "https://www.viki.com/users/{}/about",
-    "MyAnimeList": "https://myanimelist.net/profile/{}",
-    "AniList": "https://anilist.co/user/{}",
-    "Trakt": "https://trakt.tv/users/{}",
-    "RAWG": "https://rawg.io/@{}",
-    "Pastebin": "https://pastebin.com/u/{}",
-    "Paste.ee": "https://paste.ee/u/{}",
-    "IFTTT": "https://ifttt.com/p/{}",
-    "Trello": "https://trello.com/{}",
-    "Notion Site": "https://{}.notion.site",
-    "Miro": "https://miro.com/app/board/{}",
-    "Figma Community": "https://www.figma.com/@{}",
-    "Sketchfab": "https://sketchfab.com/{}",
-    "Polywork": "https://www.polywork.com/{}",
-    "Peerlist": "https://peerlist.io/{}",
-    "ResearchGate": "https://www.researchgate.net/profile/{}",
-    "ORCID": "https://orcid.org/{}",
-    "Academia": "https://independent.academia.edu/{}",
-    "Google Scholar": "https://scholar.google.com/citations?user={}",
-    "OpenSea": "https://opensea.io/{}",
-    "Giters": "https://giters.com/{}",
-    "LibraryThing": "https://www.librarything.com/profile/{}",
-    "Houzz": "https://www.houzz.com/user/{}",
-    "Ello": "https://ello.co/{}",
-    "BuySellAds": "https://www.buysellads.com/{}",
-    "SlideShare": "https://www.slideshare.net/{}",
-    "Scribd": "https://www.scribd.com/{}",
-    "Issuu": "https://issuu.com/{}",
-    "Devpost": "https://devpost.com/{}",
-    "Kofi Shop": "https://ko-fi.com/{}",
-    "Battle.net": "https://worldofwarcraft.blizzard.com/en-us/search?q={}",
-    "NameMC": "https://namemc.com/profile/{}",
-    "NationStates": "https://www.nationstates.net/nation={}",
-    "ReverbNation": "https://www.reverbnation.com/{}",
-    "Smule": "https://www.smule.com/{}",
-    "Polarsteps": "https://www.polarsteps.com/{}",
-    "Codecademy": "https://www.codecademy.com/profiles/{}",
-    "Erome": "https://www.erome.com/{}",
-    "Flipboard": "https://flipboard.com/@{}",
-    "DailyMotion Legacy": "https://www.dailymotion.com/{}",
-    "Bento": "https://bento.me/{}",
-    "Pearltrees": "https://www.pearltrees.com/{}",
-    "Shutterstock": "https://www.shutterstock.com/g/{}",
-    "EyeEm": "https://www.eyeem.com/u/{}",
-    "Civitai": "https://civitai.com/user/{}",
-    "Muck Rack": "https://muckrack.com/{}",
-    "Giphy": "https://giphy.com/{}",
-    "Thingiverse": "https://www.thingiverse.com/{}/designs",
-    "Printables": "https://www.printables.com/@{}",
-    "Myspace": "https://myspace.com/{}",
+    "Reddit": "https://www.reddit.com/user/{}",
+    "GitHub": "https://github.com/{}",
+    "Instagram": "https://www.instagram.com/{}/",
+    "TikTok": "https://www.tiktok.com/@{}",
 }
 
+# Negative markers must be very specific to avoid false negatives
 NEGATIVE_MARKERS = [
-    "page not found", "404", "user not found", "doesn't exist",
-    "nobody on reddit goes by that name", "profile not found",
-    "this account is private", "not found", "steam community :: error"
+    "nobody on reddit goes by that name", 
+    "this account has been suspended",
+    "404 not found"
 ]
 
 @dataclass
@@ -233,63 +33,64 @@ class ScanResult:
     negative_score: int = 0
     friendly_reason: str = ""
 
-# --- HELPERS ---
-
-def results_summary(results: List[ScanResult]) -> Dict[str, int]:
-    return {
-        "found": sum(1 for r in results if r.state == "found"),
-        "not_found": sum(1 for r in results if r.state == "not_found"),
-        "unconfirmed": sum(1 for r in results if r.state == "unconfirmed"),
-        "total": len(results)
-    }
-
-def humanize_reason(note: str, state: str, pos: int, neg: int) -> str:
-    if state == "found": return f"Match confirmed (Score: {pos})"
-    if state == "not_found": return "Account does not exist."
-    return note if note else "Ambiguous signals"
-
-# --- ENGINE ---
-
 def score_response(platform: str, username: str, html: str, status_code: int, final_url: str) -> tuple[str, str, str, int, int]:
     body = html.lower()
     uname_l = username.lower()
     
-    # 1. Status Check
-    if status_code in (404, 410):
+    # 1. Platform-Specific "Fingerprints" for Positive Matches
+    pos = 0
+    
+    # Twitch: Look for unique 'channelID' or 'streamer' keywords in the JS
+    if platform == "Twitch":
+        if any(x in body for x in ["channelid", "isavailable", 'login":"' + uname_l]):
+            pos += 90
+            
+    # YouTube: Look for the specific @handle in the metadata
+    if platform == "YouTube":
+        if f"youtube.com/@{uname_l}" in body or "channelid" in body:
+            pos += 90
+
+    # Reddit: Look for 'karma' or 'cake day'
+    if platform == "Reddit":
+        if any(x in body for x in ["karma", "cake day", "comment-karma"]):
+            pos += 90
+
+    # General Fallbacks
+    if uname_l in body: pos += 20
+    if uname_l in final_url: pos += 20
+
+    # 2. Re-evaluating 403s
+    # If we get a 403 but the username is in the HTML, it's a "Found" account behind a bot-block
+    if status_code == 403 and pos > 40:
+        return "found", "Profile detected despite bot-block (403)", "medium", pos, 0
+
+    # 3. Hard Negatives
+    if status_code == 404:
+        # Check if it's a 'Fake' 404 (Twitch does this sometimes)
+        if pos > 70: return "found", "Confirmed via Metadata", "high", pos, 0
         return "not_found", "HTTP 404", "high", 0, 100
 
-    # 2. Specific Soft 404 Checks (Priority)
     for marker in NEGATIVE_MARKERS:
         if marker in body:
-            return "not_found", f"Marker detected: {marker}", "high", 0, 100
+            return "not_found", f"Confirmed: {marker}", "high", 0, 100
 
-    # 3. Positive Signals
-    pos = 0
-    # YouTube fix: Check for handle in source
-    if platform == "YouTube" and f"@{uname_l}" in body: 
-        pos += 90
-    
-    # Reddit fix: Check for 'created' or 'karma' to confirm active profile
-    if platform == "Reddit" and any(x in body for x in ["karma", "cake day", "posts"]):
-        pos += 50
-
-    if uname_l in body: pos += 30
-    if uname_l in final_url: pos += 20
-    
-    if pos >= 55:
-        return "found", "Active profile indicators", "high", pos, 0
+    if pos >= 60:
+        return "found", "Matches found", "high", pos, 0
         
-    return "unconfirmed", "Could not verify presence", "low", pos, 0
+    return "unconfirmed", "Ambiguous signals", "low", pos, 0
 
-def check_platform(platform: str, username: str, timeout: float = 10.0) -> ScanResult:
+def check_platform(platform: str, username: str, timeout: float = 12.0) -> ScanResult:
     url = PLATFORMS[platform].format(username)
     try:
-        # FIXED: Changed follow_redirects to allow_redirects
+        # We rotate the impersonation target to see if it helps
+        target = "chrome110" if platform != "Twitch" else "safari_ios"
+        
         response = requests.get(
             url, 
-            impersonate="chrome110", 
+            impersonate=target, 
             timeout=timeout, 
-            allow_redirects=True
+            allow_redirects=True,
+            headers={"Referer": "https://www.google.com/"} # Adding a referer helps
         )
         
         state, note, conf, pos, neg = score_response(
@@ -302,10 +103,9 @@ def check_platform(platform: str, username: str, timeout: float = 10.0) -> ScanR
             note=note, positive_score=pos, negative_score=neg
         )
     except Exception as e:
-        # Capture the error message for debugging in the Streamlit UI
         return ScanResult(platform, url, url, "unconfirmed", None, "low", f"Error: {str(e)}")
 
-def scan_username(username: str, timeout: float = 10.0, workers: int = 20) -> List[ScanResult]:
+def scan_username(username: str, timeout: float = 12.0, workers: int = 20) -> List[ScanResult]:
     results = []
     with ThreadPoolExecutor(max_workers=workers) as executor:
         futures = {executor.submit(check_platform, p, username, timeout): p for p in PLATFORMS}
@@ -315,3 +115,16 @@ def scan_username(username: str, timeout: float = 10.0, workers: int = 20) -> Li
     order = {"found": 0, "unconfirmed": 1, "not_found": 2}
     results.sort(key=lambda x: (order.get(x.state, 3), x.platform))
     return results
+
+def results_summary(results: List[ScanResult]) -> Dict[str, int]:
+    return {
+        "found": sum(1 for r in results if r.state == "found"),
+        "not_found": sum(1 for r in results if r.state == "not_found"),
+        "unconfirmed": sum(1 for r in results if r.state == "unconfirmed"),
+        "total": len(results)
+    }
+
+def humanize_reason(note: str, state: str, pos: int, neg: int) -> str:
+    if state == "found": return f"Presence confirmed (Score: {pos})"
+    if state == "not_found": return "Account definitely not found."
+    return note if note else "Checking platform constraints..."
